bc8cb9f70d8bb03f8113f97552890dcdd5c627e6
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 15:33:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
     "Fizz"
   elsif num % 5 == 0
     "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
   end
 end
