f506d5cbeb3b3265d060939c4cb5f54ddfd330e1
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 15:16:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(num)
+  if num % 3 == 0
     "Fizz"
   end
 end
 
-def fizzbuzz(int)
-  if int % 5 == 0
+def fizzbuzz(num)
+  if num % 5 == 0
     "Buzz"
   end
 end
